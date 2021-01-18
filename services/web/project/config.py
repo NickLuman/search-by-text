fb09fb9db9308294622005aede5fee763b1219a0
Nikita Lupenko <nicklupman@gmail.com>
2021-01-18 22:41:47 +0300

    environment variables moved to .env.dev

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -3,6 +3,6 @@ from os import path, getenv
 basedir = path.abspath(path.dirname(__file__))
 
 class Config(object):
-    DEBUG = True 
+    DEBUG = bool(getenv("DEBUG", False)) 
     SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URL", "sqlite://")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = bool(getenv("SQA_TRACK_MODIFICATIONS", False)) 
